3beb46c38dc62b34d90443302bcd01955cfe1f88
Eric Nguyen <33969138+airicbear@users.noreply.github.com>
2018-12-17 15:33:28 -0500

    Update everything

--- js/bts.js
+++ js/bts.js
@@ -12,38 +12,45 @@ let btsInfo = {
   "V": {
     "name": "Kim Tae Hyung (김태형)",
     "role": "Dancer, Vocalist, Visual",
-    "birthday": "December 30, 1995",
+    "birthday": "1995-12-30",
   },
   "Suga": {
     "name": "Min Yoon Gi (민윤기)",
     "role": "Rapper",
-    "birthday": "March 9, 1993",
+    "birthday": "1993-03-09",
   },
   "Jungkook": {
     "name": "Jeon Jeong-guk (전정국)",
     "role": "Dancer, Vocalist, Visual",
-    "birthday": "September 1, 1997",
+    "birthday": "1997-09-01",
   },
   "RM": {
     "name": "Kim Nam Joon (김남준)",
     "role": "Leader, Rapper",
-    "birthday": "September 12, 1994",
+    "birthday": "1994-09-12",
   },
   "J-Hope": {
     "name": "Jung Ho Seok (정호석)",
     "role": "Dancer, Rapper",
-    "birthday": "February 18, 1994",
+    "birthday": "1994-02-18",
   },
   "Jimin": {
     "name": "Park Ji Min (박지민)",
     "role": "Dancer, Vocalist",
-    "birthday": "October 13, 1995",
+    "birthday": "1995-10-13",
   },
   "Jin": {
     "name": "Kim Seok Jin (김석진)",
     "role": "Visual, Vocalist",
-    "birthday": "December 4, 1992",
+    "birthday": "1992-12-04",
   },
+}
+
+function Button(id = "", className = "") {
+  let button = document.createElement("button");
+  button.id = id;
+  button.className = className;
+  return button;
 }
 
 function Div(id = "", className = "") {
@@ -61,70 +68,100 @@ function Text(text = "", className = "", id = "") {
   return p;
 }
 
+// https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd
+function calculateAge(birthday) {
+  let birthDate = new Date(birthday);
+  let ageDifMs = Date.now() - birthDate.getTime();
+  let ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
 function Info(memberName, infoList = btsInfo) {
   let memberInfo = infoList[memberName];
+  memberInfo["age"] = calculateAge(memberInfo["birthday"]);
   let div = Div(memberName + "Info", "memberInfo");
+  div.tabIndex = 0;
   for (let info in memberInfo) {
-    let p = Text(info.replace(/([A-Z]+)/g, " $1").replace(/([A-Z][a-z])/g, " $1").replace(/\w\S*/g, function (txt) {
+    let propertyText = info.replace(/([A-Z]+)/g, " $1").replace(/([A-Z][a-z])/g, " $1").replace(/\w\S*/g, function (txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }) + ": " + memberInfo[info], info, memberName + info);
+    });
+    let valueText = memberInfo[info];
+    let propertyClass = "member" + info.replace(/([A-Z]+)/g, "$1").replace(/([A-Z][a-z])/g, "$1").replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+    let p = Text(propertyText + ": " + valueText, propertyClass, memberName + info);
+
     div.appendChild(p);
   }
   return div;
 }
 
-function toggle(member, members, initHeight = (100 / btsMembers.length) + "vh") {
-  let hideMember = (member) => {
-    member.style.height = initHeight;
-    member.style.filter = "grayscale(100%) brightness(50%)";
-    document.getElementById(member.id + "Label").style.display = "none";
-    document.getElementById(member.id + "Info").style.display = "none";
+function showMember(member) {
+  member.style.height = "50vh";
+  member.style.filter = "grayscale(0%) brightness(100%)";
+  document.getElementById(member.id + "Label").style.display = "inline";
+  document.getElementById(member.id + "Info").style.display = "inline";
+  member.collapsed = false;
+}
+
+function collapseProfile(member, initHeight = (100 / btsMembers.length) + "vh") {
+  member.style.height = initHeight;
+  member.style.filter = "grayscale(100%) brightness(50%)";
+  document.getElementById(member.id + "Label").style.display = "none";
+  document.getElementById(member.id + "Info").style.display = "none";
+  member.collapsed = true;
+}
+
+function collapseAllProfiles(profileClassName = "Profile") {
+  let profiles = document.getElementsByClassName(profileClassName);
+  for (let i = 0; i < profiles.length; i++) {
+    collapseProfile(profiles[i]);
   }
+}
+
+function collapseAllProfilesExcept(member, members) {
   for (let i = 0; i < members.length; i++) {
     if (members[i].id !== member.id) {
-      hideMember(members[i]);
+      collapseProfile(members[i]);
     }
   }
-  if (member.style.height !== "50vh") {
-    member.style.height = "50vh";
-    member.style.filter = "grayscale(0%) brightness(100%)";
-    document.getElementById(member.id + "Label").style.display = "inline";
-    document.getElementById(member.id + "Info").style.display = "inline";
+}
+
+function toggle(member, members) {
+  collapseAllProfilesExcept(member, members);
+  if (member.collapsed) {
+    showMember(member);
   } else {
-    hideMember(member);
+    collapseProfile(member);
   }
 }
 
 function Profile(memberName = "Jungkook") {
-  let div = Div(memberName, "Profile");
+  let button = Button(memberName, "Profile");
+  button.textContent = memberName + " Profile";
 
   // Profile text
   let label = Text(memberName, "memberLabel", memberName + "Label");
   let info = Info(memberName);
-  div.appendChild(label);
-  div.appendChild(info);
+  button.appendChild(label);
+  button.appendChild(info);
 
-  // Profile image
-  div.style.backgroundImage = "url('./img/" + memberName + ".png')";
+  let bgImage = (fileExtension) => "url('./img/" + fileExtension + "/" + memberName + "." + fileExtension + "')";
+  button.style.backgroundImage = bgImage("webp") + ", " + bgImage("png"); // + ", " + bgImage("jpg");
 
-  // Open profile
-  let showMember = () => {
-    toggle(div, document.body.getElementsByClassName("Profile"));
+  let openProfile = () => {
+    toggle(button, document.body.getElementsByClassName("Profile"));
     setTimeout(() => {
-      div.scrollIntoView({
+      button.scrollIntoView({
         behavior: 'smooth',
-        block:  'center',
+        block: 'center',
       });
     }, 100);
   }
 
-  div.addEventListener("click", showMember);
+  button.addEventListener("click", openProfile);
 
-  // Make profiles focusable
-  // div.addEventListener("focus", showMember);
-  div.tabIndex = 0;
-
-  return div;
+  return button;
 }
 
 function Profiles(memberList = btsMembers) {
